79b1966ee598c6b33a41cbec172056124051ba54
SHbrai94 <albraiqishayma@gmail.com>
2026-08-26 16:57:43 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-}
+})
